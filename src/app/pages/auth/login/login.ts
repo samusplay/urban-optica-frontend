c1cd07e88444e965d95e57e6df47e32c4d68658b
samusplay <samusplay08@gmail.com>
2026-01-19 19:22:19 -0500

    login terminado

--- src/app/pages/auth/login/login.ts
+++ src/app/pages/auth/login/login.ts
@@ -1,12 +1,74 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '../services/auth.service';
+import { CommonModule } from '@angular/common';
+import { Router, RouterModule } from '@angular/router';
+import { toast } from 'ngx-sonner';
 
 @Component({
   selector: 'app-login',
-  standalone:true,
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule, FormsModule, RouterModule],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
 export class LoginComponent {
+  //instacniamos formulario
+  form: FormGroup;
+  //estado 
+  isLoading = false;
+
+  //Inyeccion de dependencias
+  constructor(
+    private readonly fb: FormBuilder,
+    private readonly authservice: AuthService,
+    private readonly router: Router
+
+  ) {
+    //Inicializamos el formulario
+    this.form = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+  //Envio de datos
+  onSubmit(): void {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      toast.warning('Por favor, ingresa credenciales válidas')
+      return;
+    }
+    //Cambiamos el estado
+    this.isLoading = true;
+    const toastId = toast.loading('Verificando credenciales...');
+
+    //Llamamos al servicio
+    this.authservice.login(this.form.value).subscribe({
+      next: (response) => {
+        this.isLoading = false;
+        toast.dismiss(toastId)
+        toast.success(`¡Bienvenido de nuevo, ${response.nombre}!`)
+
+        //Redirigimos al home/luego A users
+        this.router.navigate(['/home'])
+      },
+      error: (err) => {
+        this.isLoading = false;
+        toast.dismiss(toastId);
+        //Manejamos errores
+        const message = err.status === 401
+          ? 'Correo o contraseña incorrectos'
+          : err.error?.message || 'Error al iniciar sesión';
+
+        toast.error(message);
+        console.error('Error en Login:', err);
+      }
+    });
+  }
+  //Usamos helper
+  get f() {
+    return this.form.controls;
+  }
+
 
 }
--- src/app/theme/auth-layout/auth-layout.ts
+++ src/app/theme/auth-layout/auth-layout.ts
@@ -1,19 +1,31 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { Router, RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterLink, RouterOutlet } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-auth-layout',
-  imports: [RouterOutlet,CommonModule],
+  imports: [RouterOutlet, CommonModule,RouterLink],
   templateUrl: './auth-layout.html',
   styleUrl: './auth-layout.scss',
 })
 export class AuthLayout {
+  currentUrl: string = '';
 
-  constructor(private router: Router) {}
+  constructor(private router: Router) {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: any) => {
+      // Aquí actualizamos la variable que el HTML está mirando
+      this.currentUrl = event.url; 
+    });
+  }
 
-  //Para redirigir 
-isLoginRoute(): boolean {
-  return this.router.url.includes('login');
+  isLoginRoute(): boolean {
+    // CAMBIO AQUÍ: Usa la variable de clase, no el router directamente
+    return this.currentUrl.includes('login');
+  }
 }
-}
+
+
+
