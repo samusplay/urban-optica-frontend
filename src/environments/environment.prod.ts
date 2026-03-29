9f553fb7bc76bec9ed3af05132d403fc1829a387
samuel <samusplay08@gmail.com>
2026-03-29 18:37:37 -0500

    produccion configuracion urban plus

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+
+export const environment = {
+  production: true,
+  //Url Railway del  backend
+  apiUrl: 'optica-urban-plus-production.up.railway.app',
+  // Tu bucket de S3
+  s3Url: 'https://imagenes-lentes-contacto.s3.us-east-1.amazonaws.com'
+};
