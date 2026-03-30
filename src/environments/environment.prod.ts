dd107ab9544dc34c144552c73b50fc33ab6ec428
samuel <samusplay08@gmail.com>
2026-03-29 19:11:16 -0500

    fix url railway

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,8 @@
-
 export const environment = {
-  production: true,
-  //Url Railway del  backend
-  apiUrl: 'optica-urban-plus-production.up.railway.app',
-  // Tu bucket de S3
-  s3Url: 'https://imagenes-lentes-contacto.s3.us-east-1.amazonaws.com'
+    production: true,
+    //Url Railway del  backend
+    apiUrl: 'https://optica-urban-plus-production.up.railway.app',
+    // Tu bucket de S3
+    s3Url: 'https://imagenes-lentes-contacto.s3.us-east-1.amazonaws.com',
 };
 // hola vercel
